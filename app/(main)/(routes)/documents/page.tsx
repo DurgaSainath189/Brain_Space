7a82d977d6f52cd73ada33cadb313643c43d3331
Durga Sainath Dora <durgasainathdora@gmail.com>
2024-06-17 21:29:55 +0530

    done with the user profile

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>This is documents page</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/women_reading_light.svg"
+        width="300"
+        height="300"
+        alt="Women Reading"
+        className="dark:hidden"
+      />
+      <Image
+        src="/women_reading_dark.svg"
+        width="300"
+        height="300"
+        alt="Women Reading"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">Hello, Welcome to Brain Space</h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
