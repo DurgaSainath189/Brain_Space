0a84ae4f1954cb6c760efa1d9533b271512f698e
Durga Sainath Dora <durgasainathdora@gmail.com>
2024-06-22 12:16:00 +0530

    modified the editor component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -6,6 +6,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
+import Editor from "@/components/app";
 
 interface DocumentIdPageProps {
   params: {
@@ -43,6 +44,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor/>
       </div>
     </div>
   );
